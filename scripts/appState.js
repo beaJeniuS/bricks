284f357c72fb90d5d1e465a3d3a2b0c6e7841829
beaJeniuS <beajenius@yandex.ru>
2026-03-04 20:41:22 +0300

    add appstate basic functionality

--- scripts/appState.js
+++ scripts/appState.js
@@ -80,4 +80,102 @@ class AppState {
   set design(design) {
     return (this._currentLayout.design = [...design]);
   }
+
+  addLayout(layout) {
+    this._layouts.push(layout);
+    this._currentLayout = layout;
+    const layoutsKey = localStorage.getItem("layouts");
+    if (layoutsKey) {
+      let layouts;
+      layouts = JSON.parse(layoutsKey).map((lo) => new Layout(lo));
+      layouts.push(layout);
+      localStorage.setItem("layouts", JSON.stringify(layouts));
+    }
+    localStorage.setItem("currentLayout", layout.name);
+  }
+
+  deleteCurrentLayout() {
+    console.log(this._layouts);
+    console.log(
+      this._layouts.filter((layout) => layout !== this._currentLayout),
+    );
+    this._layouts = this._layouts.filter(
+      (layout) => layout !== this._currentLayout,
+    );
+    this._currentLayout = {};
+    localStorage.setItem("layouts", JSON.stringify(this._layouts));
+  }
+
+  saveLayouts() {
+    localStorage.setItem("currentLayout", this.layoutName);
+    localStorage.setItem("layouts", JSON.stringify(this._layouts));
+  }
+
+  loadLayouts() {
+    const curLayoutName = localStorage.getItem("currentLayout") || "";
+    const layouts = localStorage.getItem("layouts");
+    if (layouts) {
+      this._layouts = JSON.parse(layouts).map((layout) => new Layout(layout));
+    } else {
+      this._layouts.push(new Layout());
+      this._currentLayout = this._layouts[0];
+      localStorage.setItem("layouts", JSON.stringify(this._layouts));
+      localStorage.setItem("currentLayout", this._currentLayout.name);
+    }
+    this._currentLayout =
+      this.layouts.find((el) => el.name === curLayoutName) || new Layout();
+  }
+
+  getLayoutNames() {
+    return this._layouts.map((layout) => layout.name);
+  }
+
+  addColor(color) {
+    this._currentLayout.colors.push(color);
+  }
+
+  removeColor(color) {
+    this._currentLayout.colors = this._currentLayout.colors.filter(
+      (colorEl) => colorEl !== color,
+    );
+    this._currentLayout.design = this._currentLayout.design.map((desEl) =>
+      desEl.map((el) => (el == color ? 0 : el)),
+    );
+  }
+
+  saveCurrentLayout() {
+    const savedLayouts = JSON.parse(localStorage.getItem("layouts") || "[]");
+    let currentInList = savedLayouts.find(
+      (el) => el.name === this._currentLayout.name,
+    );
+    if (currentInList) {
+      currentInList.colors = [...this._currentLayout.colors];
+      currentInList.rowsCount = this._currentLayout.rowsCount;
+      currentInList.bricksCount = this._currentLayout.bricksCount;
+      currentInList.showNumbers = this._currentLayout.showNumbers;
+      currentInList.design = [...this._currentLayout.design];
+      localStorage.setItem("layouts", JSON.stringify(savedLayouts));
+    } else {
+      savedLayouts.push(this._currentLayout.name);
+      localStorage.setItem("layouts", JSON.stringify(savedLayouts));
+    }
+  }
+
+  removeCurrentLayout() {
+    this._layouts = this._layouts.filter(
+      (layout) => layout !== this._currentLayout,
+    );
+    this._currentLayout = {};
+    localStorage.setItem("layouts", JSON.stringify(this._layouts));
+    localStorage.setItem("currentLayout", "");
+  }
+
+  updateBrickColor(row, column, colorIndex) {
+    this._currentLayout.design[row][column] = colorIndex;
+  }
+
+  changeCurrentLayoutTo(layoutName) {
+    const current = this._layouts.find((layout) => layout.name === layoutName);
+    if (current) this._currentLayout = current;
+  }
 }
