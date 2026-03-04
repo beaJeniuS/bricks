a9811e81544abb42e332038b23c3bedad3d760f8
beaJeniuS <beajenius@yandex.ru>
2026-03-04 15:51:18 +0300

    add layout class constructor

--- scripts/layout.js
+++ scripts/layout.js
@@ -5,6 +5,23 @@ class Layout {
   bricksCount; // count bricks per line
   showNumbers; // show color index at every brick option
   design; // lines and bricks colors configuration
+  constructor(layout) {
+    if (layout === undefined) {
+      this.name = "по умолчанию";
+      this.colors = ["1", "2", "3", "4", "5"];
+      this.rowsCount = 10;
+      this.bricksCount = 10;
+      this.showNumbers = false;
+      this.design = [];
+    } else {
+      this.name = layout.name;
+      this.colors = [...layout.colors];
+      this.rowsCount = layout.rowsCount;
+      this.bricksCount = layout.bricksCount;
+      this.showNumbers = layout.showNumbers;
+      this.design = [...layout.design];
+    }
+  }
 }
 
 export default Layout;
