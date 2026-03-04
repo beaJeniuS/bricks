54102536cd0fbe1a803b54069a24a6fd89919d39
beaJeniuS <beajenius@yandex.ru>
2026-03-04 20:38:56 +0300

    add appstate class basic getters and setters

--- scripts/appState.js
+++ scripts/appState.js
@@ -9,6 +9,75 @@ class AppState {
     this.loadLayouts();
 
     this._currentLayout =
-      this.layouts.find((el) => el.name === curLayoutName) || new Layout();
+      this._layouts.find((el) => el.name === curLayoutName) || new Layout();
+  }
+
+  get currentLayout() {
+    return this._currentLayout;
+  }
+
+  set currentLayout(layoutName) {
+    this._currentLayout =
+      this._layouts.find((layout) => layout.name === layoutName) ||
+      new Layout();
+  }
+
+  get layoutName() {
+    return this._currentLayout.name;
+  }
+
+  get layoutsNames() {
+    return this._layouts.map((layout) => layout.name);
+  }
+
+  get layouts() {
+    return [...this._layouts];
+  }
+
+  set layouts(layouts) {
+    this._layouts = [...layouts];
+  }
+
+  get rowsCount() {
+    return this._currentLayout.rowsCount;
+  }
+
+  set rowsCount(rowsCount) {
+    this._currentLayout.rowsCount = rowsCount;
+    this._currentLayout.design = [];
+  }
+
+  get bricksCount() {
+    return this._currentLayout.bricksCount;
+  }
+
+  set bricksCount(bricksCount) {
+    this._currentLayout.bricksCount = bricksCount;
+    this._currentLayout.design = [];
+  }
+
+  get colors() {
+    return [...this._currentLayout.colors];
+  }
+
+  set colors(colors) {
+    this._currentLayout.colors = [...colors];
+    this._currentLayout.design = [];
+  }
+
+  get showNumbers() {
+    return this._currentLayout.showNumbers;
+  }
+
+  set showNumbers(showNumbers) {
+    this._currentLayout.showNumbers = showNumbers;
+  }
+
+  get design() {
+    return [...this._currentLayout.design];
+  }
+
+  set design(design) {
+    return (this._currentLayout.design = [...design]);
   }
 }
