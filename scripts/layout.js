4ae802d52b84c1b33659abc4141bcc5d1070144f
beaJeniuS <beajenius@yandex.ru>
2026-03-04 15:40:42 +0300

    add layout class

---
+++ scripts/layout.js
@@ -0,0 +1,10 @@
+class Layout {
+  name; // layout`s name
+  colors; // bricks colors indexes array
+  rowsCount; // count bricks lines
+  bricksCount; // count bricks per line
+  showNumbers; // show color index at every brick option
+  design; // lines and bricks colors configuration
+}
+
+export default Layout;
